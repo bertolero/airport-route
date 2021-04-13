c56b99c00eb633ba6394e003602348b9da655892
Gabriel Pinheiro <bertol.pinheiro@gmail.com>
2021-04-12 21:42:03 -0300

    Add command line UI as a thread.

--- src/main/java/br/com/bertol/Router.java
+++ src/main/java/br/com/bertol/Router.java
@@ -1,55 +1,16 @@
 package br.com.bertol;
 
-import br.com.bertol.input.InputReader;
-import br.com.bertol.search.RouteSearcher;
-
-import java.util.Scanner;
+import br.com.bertol.ui.CommandLine;
 
 public class Router {
-
-    public static final String EXIT_COMMAND = "q";
 
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage is 'java -jar  <file with routes>.csv'");
             System.exit(1);
         }
-        final var scanner = new Scanner(System.in);
-        try {
-            final var inputReader = new InputReader(args[0]);
-            final var routes = inputReader.getRoutes();
-            final var routeSearcher = new RouteSearcher(routes);
-            System.out.println("Type 'q' to quit");
-            while (true) {
-                System.out.print("please enter the route: ");
-                final var input = scanner.nextLine();
 
-                // check exit first
-                if (EXIT_COMMAND.equalsIgnoreCase(input)) {
-                    System.out.println("Exiting.");
-                    System.exit(0);
-                }
-
-                final var validInput = getSourceAndDestination(input);
-
-                final var result = routeSearcher.getSearchBestRoute(validInput[0], validInput[1]);
-
-                System.out.format("best route: %s\n", result.get());
-            }
-        } catch (final Exception e) {
-            System.err.println(e.getMessage());
-            System.exit(2);
-        }
-    }
-
-    private static String[] getSourceAndDestination(final String input) {
-        if (input == null || input.length() == 0 || !input.contains("-")) {
-            throw new RuntimeException("Input strung must be 'ORIGIN-DESTINATION'");
-        }
-        final var sourceAndDestination = input.split("-");
-        if (sourceAndDestination.length != 2) {
-            throw new RuntimeException("Input strung must be 'ORIGIN-DESTINATION'");
-        }
-        return sourceAndDestination;
+        final var commandLine = new CommandLine(args[0]);
+        commandLine.run();
     }
 }
---
+++ src/main/java/br/com/bertol/ui/CommandLine.java
@@ -0,0 +1,58 @@
+package br.com.bertol.ui;
+
+import br.com.bertol.input.InputReader;
+import br.com.bertol.search.RouteSearcher;
+
+import java.util.Scanner;
+
+public class CommandLine implements Runnable {
+
+    private static final String EXIT_COMMAND = "q";
+
+    private final String inputFile;
+
+    public CommandLine(final String inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    @Override
+    public void run() {
+        final var scanner = new Scanner(System.in);
+        try {
+            final var inputReader = new InputReader(inputFile);
+            final var routes = inputReader.getRoutes();
+            final var routeSearcher = new RouteSearcher(routes);
+            System.out.println("Type 'q' to quit");
+            while (true) {
+                System.out.print("please enter the route: ");
+                final var input = scanner.nextLine();
+
+                // check exit first
+                if (EXIT_COMMAND.equalsIgnoreCase(input)) {
+                    System.out.println("Exiting.");
+                    System.exit(0);
+                }
+
+                final var validInput = getSourceAndDestination(input);
+
+                final var result = routeSearcher.getSearchBestRoute(validInput[0], validInput[1]);
+
+                System.out.format("best route: %s\n", result.get());
+            }
+        } catch (final Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(2);
+        }
+    }
+
+    private static String[] getSourceAndDestination(final String input) {
+        if (input == null || input.length() == 0 || !input.contains("-")) {
+            throw new RuntimeException("Input strung must be 'ORIGIN-DESTINATION'");
+        }
+        final var sourceAndDestination = input.split("-");
+        if (sourceAndDestination.length != 2) {
+            throw new RuntimeException("Input strung must be 'ORIGIN-DESTINATION'");
+        }
+        return sourceAndDestination;
+    }
+}
